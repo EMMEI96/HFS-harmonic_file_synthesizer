fadeee1a1a338c559e9429dec67d1a39e94ab45c
David-Badiane <55662004+David-Badiane@users.noreply.github.com>
2020-02-01 15:47:06 +0100

    Update project.js

--- project.js
+++ project.js
@@ -450,7 +450,7 @@ var env = {
 
 
 const impulses = ["FileConv","autoconv","impulse1","impulse2","impulse3","impulse4"];
-const impulses_ref =[myArrayBuffer2,smallBuffer,".../impulses/impulse1.mp3","/HFS-harmony_file_synthesizer/impulses/impulse.mp3",".../impulses/impulse3amp.mp3","https://github.com/EMMEI96/HFS-harmony_file_synthesizer/blob/master/impulses/impulse4.mp3"];
+const impulses_ref =[myArrayBuffer2,smallBuffer,"./impulses/impulse1.mp3","./impulses/impulse.mp3","./impulses/impulse3amp.mp3","./impulses/impulse4.mp3"];
 
  var impulsesDropdown = document.getElementById("impulsesDropdown"); 
 
